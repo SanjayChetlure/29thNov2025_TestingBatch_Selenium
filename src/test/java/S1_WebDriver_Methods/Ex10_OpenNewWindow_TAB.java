c2c22358865548579a57b960b12264cf6ecbcf6b
SanjayChetlure <sanjay.chetlure@gmail.com>
2026-02-11 08:32:51 +0530

    11th Feb 2026

---
+++ src/test/java/S1_WebDriver_Methods/Ex10_OpenNewWindow_TAB.java
@@ -0,0 +1,21 @@
+package S1_WebDriver_Methods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ex10_OpenNewWindow_TAB
+{
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+        Thread.sleep(2000);
+
+//        driver.switchTo().newWindow(WindowType.WINDOW);
+        driver.switchTo().newWindow(WindowType.TAB);
+
+        Thread.sleep(5000);
+        driver.close();
+    }
+}
